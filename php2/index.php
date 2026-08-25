e44df5e506bd7a43eb884c84d544e5fac3654ac5
clarkalgarme <algarmeclarkjustin@gmail.com>
2026-08-25 11:49:03 +0800

    Update index.php

--- php2/index.php
+++ php2/index.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <h1>Welcome!</h1>
-    <p>Welcome to our simple PHP website.</p>
+    <p style="text-align: center;">This is PHP Output 2</p>
 </div>
 
 <?php include 'includes/footer.php'; ?>
