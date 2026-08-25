02aed440098951c1179310b54711e957d8e1eb6f
clarkalgarme <algarmeclarkjustin@gmail.com>
2026-08-25 11:18:24 +0800

    Initial commit

---
+++ php2/index.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container">
+    <h1>Welcome!</h1>
+    <p>Welcome to our simple PHP website.</p>
+</div>
+
+<?php include 'includes/footer.php'; ?>
